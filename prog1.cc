4b316216cfbcf264dc025c3dcb19c90c42a0ea92
ImperiousAce <Josephash87@gmail.com>
2022-09-14 17:54:42 -0400

    Updated prog1.cc

--- prog1.cc
+++ prog1.cc
@@ -1,13 +1,30 @@
-// Don't forget to include the header block
+/*
+ *       File: prog1.cc
+ *       Author: Joe Ash
+ *       Date: 09/14/2022
+ *       Description: Lab 3 Step 2, fixing the format of the .cc program
+ */
+
 // more information on keyword const on pages 95 - 97
 // For if else format look at page 77
 
 #include<iostream> 
 #include<cstdlib>
+
 using namespace std;
-const int INT1 = 15;const int INT2 = 20; int main()
-{cout<<"Sum of"<<INT1 <<"and"<<INT2<<"is"<<INT1+INT2;cout
-<<"Product is"<<INT1*INT2<<endl;
-if(INT1<INT2) cout<<INT2 <<"is bigger";
-else cout<<INT1<<"is bigger"; return (EXIT_SUCCESS);
+
+const int INT1 = 15;
+const int INT2 = 20; 
+
+int main(){
+    cout <<"Sum of " << INT1 <<" and " << INT2 << " is " << INT1 + INT2 << endl;
+    cout <<"Product is "<< INT1 * INT2 << endl;
+
+    if (INT1 < INT2) { 
+        cout<<INT2 <<" is bigger " << endl;
+    }
+    else {
+        cout << INT1 << "is bigger " << endl; 
+    }
+    return (EXIT_SUCCESS);
 }

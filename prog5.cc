0c21701f70ed81e15dd43eede658000828aa4ec8
ImperiousAce <Josephash87@gmail.com>
2022-09-14 18:42:32 -0400

    Updated Prog5

--- prog5.cc
+++ prog5.cc
@@ -1,4 +1,13 @@
+/**
+ *       @file: prog5.cc
+ *       @author: Joe Ash
+ *       @date: 09/14/2022         \
+ *       @brief: Step 5 
+ *  
+ */
+
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 using namespace std;
 
@@ -10,10 +19,11 @@ int main()
    int fah = 56;  //declare and initialize at the same time - page 48
    int cel = 20;
 
-   ctof = 9/5  * cel + 32;
-   ftoc = 5/9 * (fah -32);
+   ctof =  cel * 9.0 / 5 + 32;
+   ftoc = (fah - 32) * 5 / 9.0;
 
-   cout << cel << " degrees Celsius in Fahrenheit is " << ctof << endl;
+
+   cout << cel << " degrees Celsius in Fahrenheit is " << setprecision(3) << ctof << endl;
    cout << fah << " degrees Fahrenheit in Celsius is " << ftoc << endl;
 
    return (EXIT_SUCCESS);

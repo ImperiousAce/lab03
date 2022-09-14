46a191dbbf1c79140a549320c70e651798b4603c
ImperiousAce <Josephash87@gmail.com>
2022-09-14 18:06:05 -0400

    Updated prog2.cc with header

--- prog2.cc
+++ prog2.cc
@@ -1,3 +1,11 @@
+/**
+ *      File: prog2.cc
+ *      Author: Joe Ash
+ *      Date: 09/14/2022
+ *      Brief: Step 2 Problem 1
+ *  
+ */
+
 // look at the beginning two lines of  "hello" 
 // program to figure out what is missing in this program
 
